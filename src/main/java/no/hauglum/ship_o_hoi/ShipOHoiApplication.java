4eef66499e576a42f4df3eb3477d9fdd44ef9e50
Sindre <sindrehauglum@gmail.com>
2025-12-25 09:04:08 +0100

    init

---
+++ src/main/java/no/hauglum/ship_o_hoi/ShipOHoiApplication.java
@@ -0,0 +1,13 @@
+package no.hauglum.ship_o_hoi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipOHoiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShipOHoiApplication.class, args);
+	}
+
+}
